0013faa2d91f017d66739591ad7e50da8f6e60a5
mateus-gsilva <mateus.gsilva95@gmail.com>
2023-11-23 23:37:02 -0300

    removed echo line

--- terraform/ecs-sg.tf
+++ terraform/ecs-sg.tf
@@ -1,9 +1,18 @@
 resource "aws_security_group" "sg" {
-  name        = "ecs-sg"
-  vpc_id      = aws_vpc.main.id
+  name   = "ecs-sg"
+  vpc_id = aws_vpc.main.id
   description = "Inbound traffic 80 from anywhere"
 
-  ingress {
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    self        = "false"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http"
+  }
+
+      ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -22,14 +31,14 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_security_group" "alb" {
-  name        = "alb_sg"
-  vpc_id      = aws_vpc.main.id
+  name   = "alb_sg"
+  vpc_id = aws_vpc.main.id
   description = "Container SG from ALB-SG"
 
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
+    ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     security_groups = [aws_security_group.sg.id]
   }
 
