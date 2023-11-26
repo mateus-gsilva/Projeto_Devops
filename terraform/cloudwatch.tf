1b2d2b4278541695a1f06f944659bba574246870
mateus-gsilva <mateus.gsilva95@gmail.com>
2023-11-26 14:36:00 -0300

    update the grid of coudwatch dashboard

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -4,135 +4,178 @@ resource "aws_cloudwatch_dashboard" "main" {
   dashboard_body = jsonencode({
     "widgets": [
         {
-            "type": "metric",
+            "height": 6,
+            "width": 7,
+            "y": 0,
             "x": 0,
-            "y": 0,
-            "width": 4,
-            "height": 6,
+            "type": "metric",
             "properties": {
-                "region": "${var.aws_region}",
+                "legend": {
+                    "position": "bottom"
+                },
+                "liveData": false,
+                "metrics": [
+                    [ { "expression": "mm1m0 * 100 / mm0m0", "id": "expr1m0", "label": "projeto-devops", "stat": "Average" } ],
+                    [ "ECS/ContainerInsights", "CpuReserved", "ClusterName", "projeto-devops", { "id": "mm0m0", "stat": "Sum", "visible": false } ],
+                    [ ".", "CpuUtilized", ".", ".", { "id": "mm1m0", "stat": "Sum", "visible": false } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
                 "title": "CPU utilization",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [
-                    [{"id": "expr1m0", "label": "${var.app_prefix}", "expression": "mm1m0 * 100 / mm0m0", "stat": "Average"}],
-                    ["ECS/ContainerInsights", "CpuReserved", "ClusterName", "${var.app_prefix}", {"id": "mm0m0", "visible": false, "stat": "Sum"}],
-                    [".", "CpuUtilized", ".", ".", {"id": "mm1m0", "visible": false, "stat": "Sum"}]
-                ],
-                "liveData": false,
-                "period": 60,
-                "yAxis": {"left": {"min": 0, "showUnits": false, "label": "Percent"}}
+                "yAxis": {
+                    "left": {
+                        "label": "Percent",
+                        "min": 0,
+                        "showUnits": false
+                    }
+                }
             }
         },
         {
-            "type": "metric",
-            "x": 4,
-            "y": 0,
-            "width": 4,
             "height": 6,
+            "width": 8,
+            "y": 0,
+            "x": 7,
+            "type": "metric",
             "properties": {
-                "region": "${var.aws_region}",
+                "legend": {
+                    "position": "bottom"
+                },
+                "liveData": false,
+                "metrics": [
+                    [ { "expression": "mm1m0 * 100 / mm0m0", "id": "expr1m0", "label": "projeto-devops", "stat": "Average" } ],
+                    [ "ECS/ContainerInsights", "MemoryReserved", "ClusterName", "projeto-devops", { "id": "mm0m0", "stat": "Sum", "visible": false } ],
+                    [ ".", "MemoryUtilized", ".", ".", { "id": "mm1m0", "stat": "Sum", "visible": false } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
                 "title": "Memory utilization",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [
-                    [{"id": "expr1m0", "label": "${var.app_prefix}", "expression": "mm1m0 * 100 / mm0m0", "stat": "Average"}],
-                    ["ECS/ContainerInsights", "MemoryReserved", "ClusterName", "${var.app_prefix}", {"id": "mm0m0", "visible": false, "stat": "Sum"}],
-                    [".", "MemoryUtilized", ".", ".", {"id": "mm1m0", "visible": false, "stat": "Sum"}]
-                ],
-                "liveData": false,
-                "period": 60,
-                "yAxis": {"left": {"min": 0, "showUnits": false, "label": "Percent"}}
+                "yAxis": {
+                    "left": {
+                        "label": "Percent",
+                        "min": 0,
+                        "showUnits": false
+                    }
+                }
             }
         },
         {
-            "type": "metric",
-            "x": 8,
-            "y": 0,
-            "width": 4,
             "height": 6,
-            "properties": {
-                "region": "${var.aws_region}",
-                "title": "Network",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [
-                    [{"id": "expr1m0", "label": "${var.app_prefix}", "expression": "mm0m0 + mm1m0", "stat": "Average"}],
-                    ["ECS/ContainerInsights", "NetworkRxBytes", "ClusterName", "${var.app_prefix}", {"id": "mm0m0", "visible": false, "stat": "Average"}],
-                    [".", "NetworkTxBytes", ".", ".", {"id": "mm1m0", "visible": false, "stat": "Average"}]
-                ],
-                "liveData": false,
-                "period": 60,
-                "yAxis": {"left": {"showUnits": false, "label": "Bytes/Second"}}
-            }
-        },
-        {
-            "type": "metric",
-            "x": 12,
+            "width": 9,
             "y": 0,
-            "width": 3,
-            "height": 6,
-            "properties": {
-                "region": "${var.aws_region}",
-                "title": "Ephemeral Storage Utilization",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [
-                    [{"id": "expr1m0", "label": "${var.app_prefix}", "expression": "mm1m0 * 100 / mm0m0", "stat": "Average"}],
-                    ["ECS/ContainerInsights", "EphemeralStorageReserved", "ClusterName", "${var.app_prefix}", {"id": "mm0m0", "visible": false, "stat": "Sum"}],
-                    [".", "EphemeralStorageUtilized", ".", ".", {"id": "mm1m0", "visible": false, "stat": "Sum"}]
-                ],
-                "liveData": false,
-                "period": 60,
-                "yAxis": {"left": {"min": 0, "showUnits": false, "label": "Percent"}}
-            }
-        },
-        {
-            "type": "metric",
             "x": 15,
-            "y": 0,
-            "width": 3,
-            "height": 6,
+            "type": "metric",
             "properties": {
-                "region": "${var.aws_region}",
-                "title": "Container Instance Count",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [["ECS/ContainerInsights", "ContainerInstanceCount", "ClusterName", "${var.app_prefix}", {"stat": "Average"}]],
+                "legend": {
+                    "position": "bottom"
+                },
                 "liveData": false,
-                "period": 60
+                "metrics": [
+                    [ { "expression": "mm0m0 + mm1m0", "id": "expr1m0", "label": "projeto-devops", "stat": "Average" } ],
+                    [ "ECS/ContainerInsights", "NetworkRxBytes", "ClusterName", "projeto-devops", { "id": "mm0m0", "stat": "Average", "visible": false } ],
+                    [ ".", "NetworkTxBytes", ".", ".", { "id": "mm1m0", "stat": "Average", "visible": false } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
+                "title": "Network",
+                "yAxis": {
+                    "left": {
+                        "label": "Bytes/Second",
+                        "showUnits": false
+                    }
+                }
             }
         },
         {
+            "height": 6,
+            "width": 6,
+            "y": 6,
+            "x": 0,
             "type": "metric",
+            "properties": {
+                "legend": {
+                    "position": "bottom"
+                },
+                "liveData": false,
+                "metrics": [
+                    [ { "expression": "mm1m0 * 100 / mm0m0", "id": "expr1m0", "label": "projeto-devops", "stat": "Average" } ],
+                    [ "ECS/ContainerInsights", "EphemeralStorageReserved", "ClusterName", "projeto-devops", { "id": "mm0m0", "stat": "Sum", "visible": false } ],
+                    [ ".", "EphemeralStorageUtilized", ".", ".", { "id": "mm1m0", "stat": "Sum", "visible": false } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
+                "title": "Ephemeral Storage Utilization",
+                "yAxis": {
+                    "left": {
+                        "label": "Percent",
+                        "min": 0,
+                        "showUnits": false
+                    }
+                }
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 6,
+            "x": 6,
+            "type": "metric",
+            "properties": {
+                "legend": {
+                    "position": "bottom"
+                },
+                "liveData": false,
+                "metrics": [
+                    [ "ECS/ContainerInsights", "ContainerInstanceCount", "ClusterName", "projeto-devops", { "stat": "Average" } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
+                "title": "Container Instance Count"
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 6,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "legend": {
+                    "position": "bottom"
+                },
+                "liveData": false,
+                "metrics": [
+                    [ "ECS/ContainerInsights", "TaskCount", "ClusterName", "projeto-devops", { "stat": "Average" } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
+                "title": "Task Count"
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 6,
             "x": 18,
-            "y": 0,
-            "width": 3,
-            "height": 6,
-            "properties": {
-                "region": "${var.aws_region}",
-                "title": "Task Count",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [["ECS/ContainerInsights", "TaskCount", "ClusterName", "${var.app_prefix}", {"stat": "Average"}]],
-                "liveData": false,
-                "period": 60
-            }
-        },
-        {
             "type": "metric",
-            "x": 21,
-            "y": 0,
-            "width": 3,
-            "height": 6,
             "properties": {
-                "region": "${var.aws_region}",
-                "title": "Service Count",
-                "legend": {"position": "bottom"},
-                "timezone": "Local",
-                "metrics": [["ECS/ContainerInsights", "ServiceCount", "ClusterName", "${var.app_prefix}", {"stat": "Average"}]],
+                "legend": {
+                    "position": "bottom"
+                },
                 "liveData": false,
-                "period": 60
+                "metrics": [
+                    [ "ECS/ContainerInsights", "ServiceCount", "ClusterName", "projeto-devops", { "stat": "Average" } ]
+                ],
+                "period": 60,
+                "region": "us-east-1",
+                "timezone": "Local",
+                "title": "Service Count"
             }
         }
     ]
