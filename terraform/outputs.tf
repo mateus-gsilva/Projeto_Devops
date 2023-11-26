0a2e8abc73a7942bc60391ebd7ffe463de5e93b4
mateus-gsilva <mateus.gsilva95@gmail.com>
2023-11-26 14:49:47 -0300

    improve the outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,25 @@
 # outputs.tf
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = [
+    aws_subnet.subnet_az1.id,
+    aws_subnet.subnet_az2.id,
+    aws_subnet.subnet_az3.id
+  ]
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.rt.id
+}
+
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.cluster.name
 }
@@ -16,10 +36,6 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.ecr.repository_url
 }
 
-output "load_balancer_dns_name" {
-  value = aws_lb.lb.dns_name
-}
-
 output "security_group_id" {
   value = aws_security_group.sg.id
 }
@@ -27,3 +43,13 @@ output "security_group_id" {
 output "alb_security_group_id" {
   value = aws_security_group.alb.id
 }
+
+output "aws_cloudwatch_dashboard_url" {
+  value = "https://${var.aws_region}.console.aws.amazon.com/cloudwatch/home?region=${var.aws_region}#dashboards/dashboard/${var.app_prefix}-dashboard"
+}
+
+output "load_balancer_dns_url" {
+  value = "http://${aws_lb.lb.dns_name}"
+}
+
+
