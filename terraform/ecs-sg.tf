14faeef91a42677ada18daeb757e696254b245f7
mateus-gsilva <mateus.gsilva95@gmail.com>
2023-12-04 21:54:22 -0300

    updated container and task definition

--- terraform/ecs-sg.tf
+++ terraform/ecs-sg.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "external" {
     description = "http"
   }
 
-  /*ingress {
+  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
@@ -20,7 +20,7 @@ resource "aws_security_group" "external" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "http"
   }
-  */
+  
 
   egress {
     from_port   = 0
